021d20615f42be86997ee59ee473ea04bdd24a5c
HAMADA Yohei <hamada_yohei@k2-vision.jp>
2018-10-16 17:14:32 +0900

    jaws用

--- php/contact.php
+++ php/contact.php
@@ -39,7 +39,7 @@ if(get_magic_quotes_gpc()) {
 // Example $address = "joe.doe@yourdomain.com";
 
 //$address = "example@example.net";
-$address = "anantabose29@gmail.com";
+$address = "info@k2-vision.jp";
 
 
 // Configuration option.
